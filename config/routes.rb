17be875986e89a9e33e4dd201c25692e6930c891
Mario Davila <mdavila0482@gmail.com>
2017-03-21 22:37:19 -0600

    ejercicioRAILS4

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/pag1'
 
+  get 'pags/pag3'
+
   root 'pags#pag1'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
